83599466dae943e32f880a2d36287407991f51b5
joseiciano <icianojose12345@aol.com>
2020-02-09 17:07:52 -0500

    need to test these functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -21,10 +21,11 @@ export const webAPI = functions.https.onRequest(main);
 app.post("/contacts", async (request, response) => {
   try {
     // Get the name and phone number from the request body
-    const { name, phoneNumber } = request.body;
+    const { name, phoneNumber, address } = request.body;
     const data = {
       name,
-      phoneNumber
+      phoneNumber,
+      address
     };
 
     // Create a new collection in the firestore db if needed, otherwise add to
@@ -46,14 +47,14 @@ app.post("/contacts", async (request, response) => {
 app.get("/contacts/:id", async (request, response) => {
   try {
     // grab the id from the http request
-    const contactID = request.params.id;
+    const contactId = request.params.id;
 
-    if (!contactID) throw new Error("Contact ID is required");
+    if (!contactId) throw new Error("Contact ID is required");
 
     // reference the contact in the contacts collection and set it to a const
     const contact = await db
       .collection("contacts")
-      .doc(contactID)
+      .doc(contactId)
       .get();
 
     if (!contact.exists) {
@@ -77,7 +78,7 @@ app.get("/contacts", async (request, response) => {
     const contactsQuerySnapshot = await db.collection("contacts").get();
 
     // create an array for the contacts to go into
-    const contacts: { id: string; data: FirebaseFirestore.DocumentData }[] = [];
+    const contacts: any[] = [];
 
     // grab every contact in the collection
     contactsQuerySnapshot.forEach(doc => {
@@ -98,32 +99,35 @@ app.get("/contacts", async (request, response) => {
 app.put("/contacts/:id", async (request, response) => {
   try {
     // get the contact id from the request param
-    const contactID = request.params.id;
+    const contactId = request.params.id;
 
     // get the name/phone number from the request object body
     const name = request.body.name;
     const phoneNumber = request.body.phoneNumber;
+    const address = request.body.address;
 
     // check if any fields are missing
-    if (!contactID) throw new Error("ID is required");
+    if (!contactId) throw new Error("ID is required");
     if (!name) throw new Error("Name is required");
     if (!phoneNumber) throw new Error("Phone number is required");
+    if (!address) throw new Error("Address is required");
 
     // create the object to send to in the request to the server
     const data = {
       name,
-      phoneNumber
+      phoneNumber,
+      address
     };
 
     // reference the document in the nosql database so that you can update it
-    const contactRef = await db
+    await db
       .collection("contacts")
-      .doc(contactID)
+      .doc(contactId)
       .set(data, { merge: true });
 
     // return the confirmation that the document changed
     response.json({
-      id: contactID,
+      id: contactId,
       data
     });
   } catch (e) {
@@ -135,19 +139,129 @@ app.put("/contacts/:id", async (request, response) => {
 app.delete("/contacts/:id", async (request, response) => {
   try {
     // Grab the contact id from the url
-    const contactID = request.params.id;
+    const contactId = request.params.id;
 
-    if (!contactID) throw new Error("ID is required");
+    if (!contactId) throw new Error("ID is required");
 
     // delete the document out of the contacts collection
     await db
       .collection("contacts")
-      .doc(contactID)
+      .doc(contactId)
       .delete();
 
     // delete confirmation response
     response.json({
-      id: contactID
+      id: contactId
+    });
+  } catch (e) {
+    response.status(500).send(e);
+  }
+});
+
+//
+//
+//
+
+// Delete all contacts
+app.delete("/contacts", async (request, response) => {
+  try {
+    // create a snapshot of the firestore db
+    const contactsQuerySnapshot = await db.collection("contacts").get();
+
+    // create an array for the contacts to go into
+    const contacts: any[] = [];
+
+    // grab every contact in the collection
+    contactsQuerySnapshot.forEach(doc => {
+      contacts.push({
+        id: doc.id,
+        data: doc.data()
+      });
+    });
+
+    contacts.forEach(async x => {
+      await db
+        .collection("contacts")
+        .doc(x.id)
+        .delete();
+    });
+
+    // // delete confirmation response
+    // response.json({
+    //   id: contactId
+    // });
+  } catch (e) {
+    response.status(500).send(e);
+  }
+});
+
+// Create a contact
+app.post("/message", async (request, response) => {
+  try {
+    // Get the name and phone number from the request body
+    const { sender, recipient, message } = request.body;
+    const data = {
+      sender,
+      recipient,
+      message
+    };
+
+    // Create a new collection in the firestore db if needed, otherwise add to
+    // the existing colleciton
+    const contactRef = await db.collection("messages").add(data);
+    const contact = await contactRef.get();
+
+    // Show what the response will be
+    response.json({
+      id: contactRef.id,
+      data: contact.data()
+    });
+  } catch (e) {
+    response.status(500).send(e);
+  }
+});
+
+// Get all messages
+app.get("/messages", async (request, response) => {
+  try {
+    // create a snapshot of the firestore db
+    const messagesQuerySnapshot = await db.collection("messages").get();
+
+    // create an array for the messages to go into
+    const messages: any[] = [];
+
+    // grab every contact in the collection
+    messagesQuerySnapshot.forEach(doc => {
+      messages.push({
+        id: doc.id,
+        data: doc.data()
+      });
+    });
+
+    // return the array of contacts to the client
+    response.json(messages);
+  } catch (e) {
+    response.status(500).send(e);
+  }
+});
+
+// Delete a single message
+app.delete("/messages/:id", async (request, response) => {
+  try {
+    // Grab the  id from the url
+    const messageId = request.params.id;
+
+    if (!messageId) throw new Error("ID is required");
+
+    // delete the document out of the contacts collection
+    await db
+      .collection("contacts")
+      .doc(messageId)
+      .delete();
+
+    // delete confirmation response
+    response.json({
+      id: messageId
     });
   } catch (e) {
     response.status(500).send(e);
